961dbce675725a44949c71d426b1da95c1793f5b
Carlos Nina Gisbert <carlonina-sif@itpm.edu.bo>
2026-06-26 10:19:22 -0400

    examen10

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
 
